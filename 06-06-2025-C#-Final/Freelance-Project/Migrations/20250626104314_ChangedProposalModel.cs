9e7752112c389db338ad832900d1c121a5ee46bf
KAVIRAJec <kavirajppm2003@gmail.com>
2025-06-30 17:26:39 +0530

    Final Task backend changes

---
+++ 06-06-2025-C#-Final/Freelance-Project/Migrations/20250626104314_ChangedProposalModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Freelance_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedProposalModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ 06-06-2025-C#-Final/Freelance-Project/Migrations/20250626105054_AddedIsRejectedInProposal.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Freelance_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsRejectedInProposal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRejected",
+                table: "Proposals",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRejected",
+                table: "Proposals");
+        }
+    }
+}
