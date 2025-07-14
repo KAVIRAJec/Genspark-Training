13f4ce95aae7e67763c34bbaab0cf8090551db49
KAVIRAJec <kavirajppm2003@gmail.com>
2025-07-14 18:58:48 +0530

    Implemented Azure KeyVault & Az Keyvault in freelance Project fix[Day51]

--- 06-06-2025-C#-Final/Freelance-Project/obj/Debug/net8.0/Freelance-Project.AssemblyInfo.cs
+++ 06-06-2025-C#-Final/Freelance-Project/obj/Debug/net8.0/Freelance-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freelance-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0cb11bb20599f8c6da0a660eb55cedb37240c7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8c4f1abd20d2a70646b4e1c5416cc98f34716d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freelance-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freelance-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
